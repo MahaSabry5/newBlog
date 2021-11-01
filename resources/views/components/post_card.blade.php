bf07e09385478b454a0f9bf73eb05080c40581c5
MahaSabry5 <maha08526@gmail.com>
2021-11-01 11:26:54 +0200

    add edits

--- resources/views/components/post_card.blade.php
+++ resources/views/components/post_card.blade.php
@@ -1,8 +1,6 @@
 @props(['post'])
 <article
     {{$attributes -> merge(['class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl '])}}>
-{{--        class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl" >--}}
-{{--      @dd($attributes)--}}
 
     <div class="py-6 px-5">
 

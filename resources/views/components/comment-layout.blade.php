17666ff102dd8b371284c02b928b6a5c54061eac
MahaSabry5 <maha08526@gmail.com>
2021-11-03 12:24:45 +0200

    design edits

--- resources/views/components/comment-layout.blade.php
+++ resources/views/components/comment-layout.blade.php
@@ -3,7 +3,7 @@
     <div class="flex-shrink-0">
         <img src="https://i.pravatar.cc/60?u={{$comment->user_id}}" width="60px" height="60px" class="rounded-xl">
     </div>
-    <div>
+    <div class="w-full">
         <header class="mb-4">
             <h3 class="font-bold">{{$comment->author->username}}</h3>
             <h3 class="text-xs">
--- resources/views/components/form/commentForm.blade.php
+++ resources/views/components/form/commentForm.blade.php
@@ -5,7 +5,7 @@
         <img src="https://i.pravatar.cc/60?u={{auth()->id()}}" width="40px" height="40px" class="rounded-full">
         <h2 class="ml-4">Want to Comment!</h2>
     </header>
-    <div class="mt-5">
+    <div class="mt-5 ">
         <textarea name="body" class="w-full text-am focus:outline-none focus:ring" rows="5" placeholder="Write a comment!!"></textarea>
         @error('body')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
